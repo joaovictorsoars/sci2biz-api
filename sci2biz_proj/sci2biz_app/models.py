7a848aea8e910d5d065f8937674b75c5b76a90b3
Rutileno Gabriel <rutileno.silva.702@ufrn.edu.br>
2024-10-06 22:48:40 -0300

    feat: começando os trabalhos

--- sci2biz_proj/sci2biz_app/models.py
+++ sci2biz_proj/sci2biz_app/models.py
@@ -1,3 +1,19 @@
+from uuid import uuid4
 from django.db import models
 
-# Create your models here.
+
+class Roles(models.Model):
+    """Model definition for Roles."""
+    role_id = models.AutoField(primary_key=True)
+    role_name = models.CharField(max_length=100, null=False)
+
+class Users(models.Model):
+    """Model definition for Users."""
+    user_id = models.BigAutoField(primary_key=True)
+    full_name = models.CharField(max_length=255, null=False)
+    email = models.EmailField(max_length=255, unique=True, null=False)
+    role_id = models.ForeignKey(Roles, on_delete=models.CASCADE, null=False)
+    is_active = models.BooleanField(default=True, null=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+    public_id = models.UUIDField(default=uuid4, editable=False, unique=True, null=False)
+    password = models.CharField(max_length=255, null=False)
